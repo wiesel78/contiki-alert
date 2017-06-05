5b06b57f66ae3275285c77f8ecb4e5d1136ea447
Philip Ullrich <wurst78@gmail.com>
2017-06-05 22:55:38 +0200

    comment ping header

--- apps/ping-service/ping-service.h
+++ apps/ping-service/ping-service.h
@@ -1,3 +1,10 @@
+/*
+    Ping-Service send periodically ping reuqests to the next hop
+
+    if it is include, its start a process.
+    the initialize method can call after PROCESS_BEGIN() in your main process
+*/
+
 #ifndef PING_SERVICE_H_
 #define PING_SERVICE_H_
 
@@ -16,16 +23,23 @@ typedef struct ping_client_config {
     int interval;
 } ping_client_config_t;
 
+/* current state of ping service */
 typedef struct ping_service_state {
     int rssi;
 } ping_service_state_t;
 
+/* periodic send of ping by this time */
 #define DEFAULT_PING_INTERVAL       (CLOCK_SECOND * 30)
 
-
+/* initialize the ping service
+ * by example, this method can call before the main loop is called
+ * @param config : pointer of config item with ping-service-settings
+ * @param state : state object with runtime informations (for example : rssi)
+ */
 extern void ping_service_init(  ping_client_config_t *config,
                                 ping_service_state_t *state);
-void ping_service_update(process_event_t ev, process_data_t data);
+
+/* send a ping manually */
 extern void ping(void);
 
 #endif /* PING_SERVICE_H_ */
